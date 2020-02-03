67638dc1922b988b6d3b71460950507c90c7008c
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-02-04 01:33:56 +0300

    Add slider parameters validation

--- src/index.js
+++ src/index.js
@@ -2,9 +2,19 @@ import Model from './Ordinary-slider/Model/Model';
 import View from './Ordinary-slider/View/View';
 import Presenter from './Ordinary-slider/Presenter/Presenter';
 
+import { isObject } from './Ordinary-slider/helpers';
+
 (function selfInvokingFunction($) {
   // eslint-disable-next-line no-param-reassign
   $.fn.oSlider = function addToJqueryPrototype(options = {}) {
+    if (!isObject(options)) {
+      throw new TypeError('Ordinary slider configuration should be an object');
+    }
+
+    if (!this.length) {
+      throw new ReferenceError('Connection to non-existent element');
+    }
+
     return this.map((i, currentElement) => {
       const model = new Model(options);
       const view = new View(currentElement, model.getState());
