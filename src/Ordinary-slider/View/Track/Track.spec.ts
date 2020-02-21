8b7c6280e178c728a7d3684a9da985fe21b2a21d
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-02-22 00:53:18 +0300

    Add Track.spec.ts

---
+++ src/Ordinary-slider/View/Track/Track.spec.ts
@@ -0,0 +1,30 @@
+import Track from './Track';
+import ViewComponent from '../ViewComponent/ViewComponent';
+
+import { TrackOptions } from './Interfaces';
+
+import { testHasElement, testHasInstance } from '../../helpers/helpers';
+
+const options: Partial<TrackOptions> = {
+  parent: document.body,
+  notify: () => true,
+  min: 0,
+  max: 100,
+  position: 0,
+  tip: true,
+};
+
+const track = new Track(options as TrackOptions);
+
+describe('Track', () => {
+  test('is an instance of class ViewComponent',
+    () => testHasInstance(track, ViewComponent));
+
+  test('should be added to parent',
+    () => testHasElement(document.body, track.getElement()));
+
+  test('contains element thumb', () => {
+    const isFounded = !!track.getElement().querySelector('.o-slider__thumb');
+    expect(isFounded).toBe(true);
+  });
+});
