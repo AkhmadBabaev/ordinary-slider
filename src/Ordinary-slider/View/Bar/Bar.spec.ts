50f3cb7254720680271c5efadb9c19609188832c
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-02-24 12:05:39 +0300

    Create Bar test

---
+++ src/Ordinary-slider/View/Bar/Bar.spec.ts
@@ -0,0 +1,36 @@
+import Bar from './Bar';
+import ViewComponent from '../ViewComponent/ViewComponent';
+
+import { BarOptions } from './Interfaces';
+
+import { testHasElement, testHasInstance } from '../../helpers/helpers';
+
+const options: BarOptions = {
+  parent: document.body,
+  isEnabled: true,
+  width: '0%',
+};
+
+// @ts-ignore
+const bar = new Bar(options);
+
+describe('Bar', () => {
+  test('is an instance of class ViewComponent',
+    () => testHasInstance(bar, ViewComponent));
+
+  test('should be added to parent',
+    () => testHasElement(document.body, bar.getElement()));
+
+  test('should be removed from parent', () => {
+    bar.update({ isEnabled: false });
+
+    const isFounded = document.body.contains(bar.getElement());
+    expect(isFounded).toBe(false);
+  });
+
+  test('handles width value', () => {
+    bar.update({ width: '10%' });
+
+    expect(bar.getElement().style.width).toBe('10%');
+  });
+});
