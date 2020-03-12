2562217e0951d7bd13be21dbfa9b1ff38d3da7e9
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-03-12 14:55:24 +0300

    Handle properties to, range in model

--- src/plugin/Model/Model.ts
+++ src/plugin/Model/Model.ts
@@ -53,6 +53,9 @@ class Model extends Observable {
     // handle gotten properties
     Object.keys(this.changes).forEach((key) => this.handleProperty(key));
 
+    // sort values From|To in changes and temporary state
+    this.sortFromTo();
+
     // notify subscribers about state changes
     Object.keys(this.changes).length && notify && this.notify(this.changes);
 
@@ -69,13 +72,22 @@ class Model extends Observable {
   }
 
   public reset(): void {
+    Object.keys(this.state).forEach((key) => {
+      !Object.prototype.hasOwnProperty.call(defaultState, key)
+        && delete this.state[key as keyof State];
+    });
+
     this.setState(defaultState);
   }
 
   private handleMin(): void {
     let { min } = this.temporaryState;
-    const { max, step, from } = this.temporaryState;
+    const {
+      max, step, from, to,
+    } = this.temporaryState;
+
     const isGreaterThanFrom = min > from;
+    const isGreaterThanTo = min > (to as number);
     const gap = max - min;
 
     min >= max && (min = max - 1);
@@ -86,16 +98,24 @@ class Model extends Observable {
 
     // update related properties
     const isStepUpdated = isDefined(this.changes.step);
-    !isStepUpdated && (step > gap) && this.handleStep();
+
 
     const isFromUpdated = isDefined(this.changes.from);
+    const isToUpdated = isDefined(this.changes.from);
+
+    !isStepUpdated && (step > gap) && this.handleStep();
     !isFromUpdated && isGreaterThanFrom && this.handleFrom();
+    !isToUpdated && isGreaterThanTo && this.handleTo();
   }
 
   private handleMax(): void {
     let { max } = this.temporaryState;
-    const { min, step, from } = this.temporaryState;
+    const {
+      min, step, from, to,
+    } = this.temporaryState;
+
     const isLessThanFrom = max < from;
+    const isLessThanTo = max < (to as number);
     const gap = max - min;
 
     max <= min && (max = min + 1);
@@ -106,10 +126,14 @@ class Model extends Observable {
 
     // update related properties
     const isStepUpdated = isDefined(this.changes.step);
-    !isStepUpdated && (step > gap) && this.handleStep();
+
 
     const isFromUpdated = isDefined(this.changes.from);
+    const isToUpdated = isDefined(this.changes.to);
+
+    !isStepUpdated && (step > gap) && this.handleStep();
     !isFromUpdated && isLessThanFrom && this.handleFrom();
+    !isToUpdated && isLessThanTo && this.handleTo();
   }
 
   private handleStep(): void {
@@ -126,11 +150,14 @@ class Model extends Observable {
 
     // update related property
     const isFromUpdated = isDefined(this.changes.from);
+    const isToUpdated = isDefined(this.changes.to);
+
     !isFromUpdated && this.handleFrom();
+    !isToUpdated && this.handleTo();
   }
 
-  private handleFrom(): void {
-    let value = this.temporaryState.from;
+  private handleValue(param: number): number {
+    let value = param;
     const { min, max, step } = this.temporaryState;
     const remainder = (value - min) % step;
 
@@ -143,10 +170,31 @@ class Model extends Observable {
 
     value < min && (value = min);
     value > max && (value = max);
-    value = softRounding(value);
+    return softRounding(value);
+  }
 
-    this.changes.from = value;
-    this.temporaryState.from = value;
+  private handleFrom(): void {
+    const from = this.handleValue(this.temporaryState.from);
+
+    this.changes.from = from;
+    this.temporaryState.from = from;
+  }
+
+  private handleTo(): void {
+    if (!this.temporaryState.range) return;
+    const to = this.handleValue(this.temporaryState.to as number);
+
+    this.changes.to = to;
+    this.temporaryState.to = to;
+  }
+
+  private handleRange(): void {
+    if (!this.temporaryState.range || isDefined(this.temporaryState.to)) return;
+
+    const to = this.handleValue(this.temporaryState.max);
+
+    this.changes.to = to;
+    this.temporaryState.to = to;
   }
 
   private validateChanges(): void {
@@ -155,6 +203,7 @@ class Model extends Observable {
 
       switch (prop) {
         case 'from':
+        case 'to':
         case 'step':
         case 'min':
         case 'max':
@@ -165,6 +214,7 @@ class Model extends Observable {
 
         case 'tip':
         case 'bar':
+        case 'range':
           if (!isBoolean(value)) throw new TypeError(`${prop} is not a boolean`); break;
 
         default: throw new Error(`${prop} is non existed property`);
@@ -174,7 +224,9 @@ class Model extends Observable {
 
   private handleProperty(property: string): void {
     switch (property) {
+      case 'range': this.handleRange(); break;
       case 'from': this.handleFrom(); break;
+      case 'to': this.handleTo(); break;
       case 'min': this.handleMin(); break;
       case 'max': this.handleMax(); break;
       case 'step': this.handleStep(); break;
@@ -187,6 +239,16 @@ class Model extends Observable {
       const value = this.changes[key as keyof State];
       this.getState()[key as keyof State] === value && delete this.changes[key as keyof State];
     });
+  }
+
+  private sortFromTo(): void {
+    if (!this.temporaryState.range) return;
+
+    const values = [this.temporaryState.from, this.temporaryState.to as number];
+    values.sort((a, b) => a - b);
+
+    [this.changes.from, this.changes.to] = values;
+    this.temporaryState = { ...this.temporaryState, ...this.changes };
   }
 }
 
