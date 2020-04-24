c3c4a82f0f74fa66a52e620a6cdb9af37685f248
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-04-24 20:57:15 +0300

    Improve View.spect.ts

--- src/plugin/View/View.spec.ts
+++ src/plugin/View/View.spec.ts
@@ -3,35 +3,50 @@ import defaultState from '../Model/default-state';
 import { hasChild } from '../helpers/helpers';
 import View from './View';
 
-document.body.innerHTML = '<div id="test"></div>';
-const testElement = document.body.querySelector('#test')!;
-const view = new View(testElement as HTMLElement, defaultState);
+document.body.innerHTML = '<div id="test" class="o-slider"></div>';
+const sliderElement = document.body.querySelector('#test')!;
+const view = new View(sliderElement as HTMLElement, defaultState);
 
 describe('View', () => {
   test('is an instance of class Observable', () => expect(view).toBeInstanceOf(Observable));
 
   test('should be merged into parent', () => {
-    expect(testElement.classList.contains('o-slider')).toBeTruthy();
+    expect(sliderElement.classList.contains('o-slider')).toBeTruthy();
   });
 
   test('contains element track', () => {
-    const track = testElement.querySelector('.o-slider__track')!;
-    expect(hasChild(testElement, track)).toBeTruthy();
+    const trackElement = sliderElement.querySelector('.o-slider__track')!;
+    expect(hasChild(sliderElement, trackElement)).toBeTruthy();
   });
 
   test('contains data attributes', () => {
     Object.keys(defaultState).forEach((attr) => {
-      expect(testElement.hasAttribute(`data-${attr}`)).toBeTruthy();
+      expect(sliderElement.hasAttribute(`data-${attr}`)).toBeTruthy();
     });
   });
 
   test('adds BEM modifier is_vertical if vertical set as true', () => {
-    view.applyState({ ...view.getOptions(), vertical: true });
-    expect(testElement.classList.contains('o-slider_is_vertical')).toBeTruthy();
+    view.applyState({ vertical: true });
+    expect(sliderElement.classList.contains('o-slider_is_vertical')).toBeTruthy();
   });
 
   test('adds BEM modifier is_horizontal if vertical set as false', () => {
-    view.applyState({ ...view.getOptions(), vertical: false });
-    expect(testElement.classList.contains('o-slider_is_horizontal')).toBeTruthy();
+    view.applyState({ vertical: false });
+    expect(sliderElement.classList.contains('o-slider_is_horizontal')).toBeTruthy();
+  });
+
+  test('getOptions returns current options', () => {
+    expect(view.getOptions()).toEqual(defaultState);
+  });
+
+  test('should react to click on the track element', () => {
+    view.notify = jest.fn();
+
+    const trackElement = sliderElement.querySelector('.o-slider__track')!;
+    trackElement.dispatchEvent(new Event('click', {
+      bubbles: true,
+    }));
+
+    expect(view.notify).toHaveBeenCalled();
   });
 });
