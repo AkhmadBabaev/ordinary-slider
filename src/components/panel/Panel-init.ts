33b20ca3a327c379b678b3cf1afd8e48d02ddcfc
AkhmadBabaev <akhmad.babaev@gmail.com>
2020-06-05 00:15:26 +0300

    Get rid of useless Object.keys

--- src/components/panel/Panel-init.ts
+++ src/components/panel/Panel-init.ts
@@ -2,5 +2,5 @@ import Panel from './Panel';
 
 window.addEventListener('load', () => {
   const panels = document.querySelectorAll('.js-panel') as NodeListOf<HTMLElement>;
-  Object.keys(panels).forEach((_, index): {} => new Panel(panels[index]));
+  panels.forEach((_, index): {} => new Panel(panels[index]));
 });
